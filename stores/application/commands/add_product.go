e9c8840e53421690bcda93b14da471d34f117051
Vladislav Sverbut <vladislavsverbut@gmail.com>
2023-02-15 01:42:33 +0300

    add commands for store microservice

---
+++ stores/application/commands/add_product.go
@@ -0,0 +1,58 @@
+package commands
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/vladesco/ecommerce-microservices/internal/ddd"
+	productDomain "github.com/vladesco/ecommerce-microservices/stores/domain/product"
+	storeDomain "github.com/vladesco/ecommerce-microservices/stores/domain/store"
+)
+
+type AddProductParams struct {
+	Id          string
+	StoreId     string
+	Name        string
+	Description string
+	SKU         string
+	Price       float64
+}
+
+type AddProductHandler struct {
+	storeRepository   storeDomain.StoreRepository
+	productRepository productDomain.ProductRepository
+	eventPublisher    ddd.EventPublisher
+}
+
+func NewAddProductHandler(
+	storeRepository storeDomain.StoreRepository,
+	productRepository productDomain.ProductRepository,
+	eventPublisher ddd.EventPublisher,
+) AddProductHandler {
+	return AddProductHandler{
+		storeRepository,
+		productRepository,
+		eventPublisher,
+	}
+}
+
+func (handler AddProductHandler) AddProduct(ctx context.Context, params AddProductParams) error {
+	if _, err := handler.storeRepository.FindOne(ctx, params.Id); err != nil {
+		return fmt.Errorf("Add product error while finding product", err)
+	}
+
+	product, err := productDomain.CreateProduct(params.Id, params.StoreId, params.Name, params.Description, params.SKU, params.Price)
+
+	if err != nil {
+		return fmt.Errorf("Add product error while creating product", err)
+	}
+
+	if err := handler.productRepository.Save(ctx, product); err != nil {
+		return fmt.Errorf("Add product error while saving product", err)
+
+	}
+
+	err = handler.eventPublisher.Publish(ctx, product.GetEvents()...)
+
+	return err
+}
--- stores/domain/store/store_repository.go
+++ stores/domain/store/store_repository.go
@@ -1,4 +1,4 @@
-package store
+package store_domain
 
 import (
 	"context"
